6821886394988107b5c89ef77532b5fcf7290438
Lucranix28 <45935449+Lucranix28@users.noreply.github.com>
2020-01-23 20:41:51 +0700

    Enter

--- Andy Lucky-13218058/Quiz 2/Quiz2.c
+++ Andy Lucky-13218058/Quiz 2/Quiz2.c
@@ -13,17 +13,17 @@ Metode : Cramer 2x2
 */
 
 int main(void) {
-    int dx, dy, det;
+    int x, y, det;
     int a[2][2] = {{2, -1}, {4, -1}};   // Persamaan 2x + -1y dan 4x + -1y
     int c[2]    = {-2, 4};              // Nilai persamaan -2 dan 4
     
     // Dengan menggunakan aturan Cramer
-    det = ((a[0][0] * a[1][1]) - (a[0][1] * a[1][0]));
-    dx = ((c[0] * a[1][1]) - (c[1] * a[0][1])) / det;
-    dy = ((a[0][0] * c[1]) - (a[1][0] * c[0])) / det;
-    
-    printf("x = %d \n", dx);
-    printf("y = %d \n", dy); 
+    det = ((a[0][0] * a[1][1]) - (a[0][1] * a[1][0]));  // Mencari Determinan dari persamaan
+    x = ((c[0] * a[1][1]) - (c[1] * a[0][1])) / det;    // Mencari Nilai x dengan aturan Cramer, x = Detx/Det
+    y = ((a[0][0] * c[1]) - (a[1][0] * c[0])) / det;    // Mencari Nilai y dengan aturan Cramer, y = Dety/Det
+
+    printf("x = %d \n", x);
+    printf("y = %d \n", y); 
     
     return (0);
 }
